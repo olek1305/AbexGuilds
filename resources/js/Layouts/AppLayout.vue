3e7f492dae64caa8859ce7888f96b0b1c0ffac6c
olek1305 <olek1305@gmail.com>
2024-02-26 12:15:48 +0100

    flash success and error

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue';
-import { Head, Link, router } from '@inertiajs/vue3';
+import {computed, ref} from 'vue';
+import {Head, Link, router, usePage} from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import Dropdown from '@/Components/Dropdown.vue';
@@ -25,6 +25,14 @@ const switchToTeam = (team) => {
 const logout = () => {
     router.post(route('logout'));
 };
+
+const flashSuccess = computed(
+    () => usePage().props.flash.success
+);
+
+const flashError = computed(
+    () => usePage().props.flash.error
+);
 </script>
 
 <template>
@@ -284,7 +292,17 @@ const logout = () => {
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main class="container mx-auto p-4 w-full">
+                    <div v-if="flashSuccess" class="p-1 mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900">
+                        {{ flashSuccess }}
+                    </div>
+
+                <div v-if="flashError" class="p-1 mb-4 border rounded-md shadow-sm border-red-200 dark:border-red-800 bg-red-50 dark:bg-red-900 dark:text-white">
+                    {{ flashError }}
+                </div>
+
+                    <slot>Default</slot>
+
                 <slot />
             </main>
         </div>
