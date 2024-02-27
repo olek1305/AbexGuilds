ccf2f5f16cf723eaf3f223ce453f9eb329cce52d
olek1305 <olek1305@gmail.com>
2024-02-27 21:46:53 +0100

    add nprogress

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,22 +2,26 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import {createInertiaApp, router} from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from 'ziggy-js';
+import NProgress from 'nprogress'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+router.on('start', () => NProgress.start());
+router.on('finish', () => NProgress.done());
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
+    progress: {
+        color: 'red',
+        showSpinner: false
+    },
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
     },
 });
