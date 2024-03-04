a4268e53e2e703ac020968201c6da9b026d7146a
olek1305 <olek1305@gmail.com>
2024-03-04 17:05:44 +0100

    change value

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Guild::factory(5)->create();
+        \App\Models\User::factory(30)->create();
+        \App\Models\Guild::factory(10)->create();
         \App\Models\Note::factory(20)->create();
         \App\Models\Season::factory(3)->create();
-        \App\Models\PlayerSeason::factory(50)->create();
+        \App\Models\PlayerSeason::factory(120)->create();
 
 
         // \App\Models\User::factory()->create([
