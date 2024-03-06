b2be95feb0b8fa82031330aae36e098a326337cb
olek1305 <olek1305@gmail.com>
2024-03-06 14:41:46 +0100

    improved UI, change css

--- resources/js/Pages/Note/Components/Note.vue
+++ resources/js/Pages/Note/Components/Note.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="dark:text-white border-2 border-sky-500 p-1">
+    <div class="dark:text-white border-2 border-sky-500 p-1 bg-amber-100 dark:bg-sky-800 flex flex-col">
         <p>Notatnik: ID {{ props.note.id }}: {{ shortCutData(props.note.created_at) }}</p>
         <p v-if="props.note.user">Gracz: {{ props.note.user.name }}</p>
-        <p>Powód: {{ props.note.title }} </p>
-        <p> {{ props.note.body }}</p>
-        <div class="flex justify-end">
-            <button @click="editNote">Edit</button>
-            <button @click="deleteNote">Delete</button>
+        <p>Powód: {{ props.note.title }}</p>
+        <p>{{ props.note.body }}</p>
+        <div class="flex justify-center">
+            <button class="btn ml-2 flex-grow" @click="editNote">Edit</button>
+            <button class="btn ml-2 flex-grow" @click="deleteNote">Delete</button>
         </div>
     </div>
 </template>
