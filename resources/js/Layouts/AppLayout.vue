9059f2fc8bddf24c43f8ed6cf74be3067f2dac2f
olek1305 <olek1305@gmail.com>
2024-03-08 13:21:00 +0100

    disabled route dashboard

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -56,9 +56,9 @@ const flashError = computed(
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
+<!--                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">-->
+<!--                                    Dashboard-->
+<!--                                </NavLink>-->
                                 <NavLink :href="route('player.index')" :active="route().current('player.index')">
                                     Players
                                 </NavLink>
@@ -147,9 +147,9 @@ const flashError = computed(
                 <!-- Responsive Navigation Menu -->
                 <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
                     <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
+<!--                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">-->
+<!--                            Dashboard-->
+<!--                        </ResponsiveNavLink>-->
                         <ResponsiveNavLink :href="route('player.index')" :active="route().current('player.index')">
                             Players
                         </ResponsiveNavLink>
