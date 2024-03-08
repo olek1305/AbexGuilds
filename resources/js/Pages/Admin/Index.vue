e92af3ab69bce49e3412899b9f262e1085449206
olek1305 <olek1305@gmail.com>
2024-03-08 13:21:51 +0100

    add route admin.user and admin.guild

--- resources/js/Pages/Admin/Index.vue
+++ resources/js/Pages/Admin/Index.vue
@@ -2,28 +2,29 @@
     <AppLayout>
         <div>
             <Box class="text grid grid-rows-5 grid-flow-col m-2 text-center">
-                <p>Sezon: {{ latestSeasonId }}</p>
+                <p class="col-span-5">Sezon: {{ latestSeasonId }}</p>
                 <div v-for="guild in guilds" :key="guild.id">
                     <p class="p-2">{{ guild.name }} - {{ guild.players_count }}</p>
                 </div>
             </Box>
             <Box class="text flex justify-center m-2 text-center ">
-                <div class="buttonDiv">
-                    <p class="p-1">Utwórz Guild</p>
+                <div class="btn" @click="routeToGuild">
+                    <span class="p-1">Utwórz Guild</span>
                 </div>
-                <div class="buttonDiv">
-                    <p class="p-1">Utwórz User</p>
+                <div class="btn" @click="routeToUser">
+                    <span class="p-1">Utwórz User</span>
                 </div>
             </Box>
             <Box class="text flex justify-center m-2 text-center ">
-                <div class="buttonDiv text-lg ">
-                    <p class="p-1">zmiana nicku gracza</p>
+                <p class="text-center">W trakcie dev</p>
+                <div class="btn text-lg">
+                    <span class="p-1 cursor-not-allowed">zmiana nicku gracza</span>
                 </div>
-                <div class="buttonDiv text-lg">
-                    <p class="p-1">zmiana hasła gracza</p>
+                <div class="btn text-lg">
+                    <span class="p-1 cursor-not-allowed">zmiana hasła gracza</span>
                 </div>
-                <div class="buttonDiv text-lg">
-                    <p class="p-1">Przywracanie user</p>
+                <div class="btn text-lg">
+                    <span class="p-1 cursor-not-allowed">Przywracanie user</span>
                 </div>
             </Box>
         </div>
@@ -32,17 +33,17 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Box from "@/Pages/Components/UI/Box.vue";
+import {route} from "ziggy-js";
 
 const props = defineProps({
     guilds: Array,
     latestSeasonId: Number
 })
+
+const routeToGuild = () => {
+    window.location.href = route('admin.guild.create');
+};
+const routeToUser = () => {
+    window.location.href = route('admin.user.create');
+};
 </script>
-
-<style scoped>
-
-.buttonDiv{
-    @apply border-2 m-1
-}
-
-</style>
