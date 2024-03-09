a85e27f70315dc0061a938654f4ab15e4d5f3f04
olek1305 <olek1305@gmail.com>
2024-03-09 15:40:26 +0100

    show info season, add checkbox

--- resources/js/Pages/Player/Edit.vue
+++ resources/js/Pages/Player/Edit.vue
@@ -7,16 +7,12 @@
                 <p>Guild: {{ player.guild.name }}</p>
                 <p>ID user: {{ player.user.id }}</p>
                 <p>ID player: {{ player.id }}</p>
-            </div>
-            <div class="mx-auto p-2 border-2 border-sky-500 rounded-md text inline-block m-1">
-                <p>Wartosc:</p>
-                <p>1 - Tak</p>
-                <p>0 - Nie</p>
+                <p>Season: {{ player.season }}</p>
             </div>
         </div>
         <form class="grid grid-cols-subgrid justify-center" @submit.prevent="update" >
             <div class="text-center text">
-                <div>
+                <div class="m-1">
                     <label class="label">Damage</label>
                     <input v-model.number="form.damage" type="text" class="text-center shadow-md text-black" />
                     <div v-if="form.errors.damage">
@@ -24,24 +20,19 @@
                     </div>
                 </div>
 
-                <div>
-                    <label class="label">Obserwator</label>
-                    <input v-model.number="form.is_observer" type="text" class="text-center shadow-md text-black" />
-                    <div v-if="form.errors.is_observer">
-                        {{ form.errors.is_observer }}
-                    </div>
+                <div class="flex items-center m-2 ps-4 border border-gray-200 rounded dark:border-gray-700">
+                    <input id="gwiazda-checkbox" type="checkbox" v-model="form.is_star" class="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600">
+                    <label for="gwiazda-checkbox" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 cursor-pointer">Gwiazda</label>
                 </div>
 
-                <div>
-                    <label class="label">Gwiazda</label>
-                    <input v-model.number="form.is_star" type="text" class="text-center shadow-md text-black" />
-                    <div v-if="form.errors.is_star">
-                        {{ form.errors.is_star }}
-                    </div>
+                <div class="flex items-center m-2 ps-4 border border-gray-200 rounded dark:border-gray-700">
+                    <input id="obserwator-checkbox" type="checkbox" v-model="form.is_observer" class="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600">
+                    <label for="obserwator-checkbox" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 cursor-pointer">Obserwator</label>
                 </div>
+
 
                 <div class="grid">
-                    <button class="btn mt-3" type="submit">Update</button>
+                    <button class="btn h-full" type="submit">Update</button>
                 </div>
             </div>
         </form>
@@ -59,11 +50,14 @@ const props = defineProps({
 
 const form = useForm({
     damage: props.player.damage,
-    is_star: props.player.is_star,
-    is_observer: props.player.is_observer,
+    is_star: !!props.player.is_star,
+    is_observer: !!props.player.is_observer,
 })
 
 const update = () => {
     form.put(route('player.update', { player: props.player.id }))
 }
+
+console.log(form.is_star);
+console.log(form.is_observer);
 </script>
