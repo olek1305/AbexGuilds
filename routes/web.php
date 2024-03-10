312d5db226c67c74617991adfa4cc7aed33cb94a
olek1305 <olek1305@gmail.com>
2024-03-10 21:06:09 +0100

    cleaned up

--- routes/web.php
+++ routes/web.php
@@ -10,30 +10,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//Route::get('/', function () {
-//    return Inertia::render('Welcome', [
-//        'canLogin' => Route::has('login'),
-//        'canRegister' => Route::has('register'),
-//    ]);
-//});
-
 Route::get('/', [PlayerSeasonController::class, 'index'])->name('player.index')->middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']);
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::resource('/player', PlayerSeasonController::class);
     Route::get('/note', [NoteController::class, 'index'])->name('note.index');
-    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');;
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::prefix('admin')->group(function () {
         Route::get('guild/create', [AdminGuildController::class, 'create'])->name('admin.guild.create');
         Route::post('guild/store', [AdminGuildController::class, 'store'])->name('admin.guild.store');
