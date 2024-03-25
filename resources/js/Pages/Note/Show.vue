262dfbfe61aebd69174cb7b72fffec8866c5af7d
olek1305 <olek1305@gmail.com>
2024-03-25 20:50:33 +0100

    add if no note then show create

--- resources/js/Pages/Note/Show.vue
+++ resources/js/Pages/Note/Show.vue
@@ -1,11 +1,18 @@
 <template>
     <AppLayout>
         <h2 class="title text-center">Notatnik użytkownika: {{ user.name }}</h2>
-        <div class="grid gap-4 sizeSmall sizeBig">
+        <div v-if="notes.data.length > 0" class="grid gap-4 sizeSmall sizeBig">
             <Note
                 v-for="note in notes.data"
                 :key="note.id" :note="note"
             />
+        </div>
+
+        <div v-else class="text-center p-5 text grid justify-center">
+            <p class="p-3">Ten użytkownik nie posiada notatek. Stwórz notatkę!</p>
+            <div>
+                <Link class="btn" method="get" as="button" type="button" :href="route('note.create')">Home</Link>
+            </div>
         </div>
         <div v-if="notes.data.length" class="w-full flex justify-center mt-4 mb-4 title">
             <Pagination :links="notes.links"/>
@@ -17,6 +24,8 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Note from "@/Pages/Note/Components/Note.vue";
 import Pagination from "@/Pages/Components/UI/Pagination.vue";
+import { Link } from '@inertiajs/vue3'
+import { route } from "ziggy-js";
 
 const props = defineProps({
     user: Object,
