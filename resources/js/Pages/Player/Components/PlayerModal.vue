4b8e88e0421a6f60fde4594574806b19e5b4528f
olek1305 <olek1305@gmail.com>
2024-03-30 21:45:39 +0100

    add modal for kick/ban

--- resources/js/Pages/Player/Components/PlayerModal.vue
+++ resources/js/Pages/Player/Components/PlayerModal.vue
@@ -1,14 +1,14 @@
 <template>
     <div v-if="showModal" @click="handleBackgroundClick" class="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-55">
         <div class="bg-white p-4 rounded shadow-md dark:bg-gray-800">
-            <div v-if="!showConfirmation">
-                <div>
+            <div v-if="!showKickConfirmation && !showBanConfirmation && !showDeleteConfirmation">
+                <Box class="m-1">
                     <h2 class="text-lg font-bold mb-2 text">Gracz: {{ props.player.user.name }}</h2>
                     <button type="button" class="bg-blue-500 hover:bg-gray-300 textbtn" @click="editPlayer(props.player)">Edit</button>
                     <button type="button" class="bg-yellow-600 hover:bg-gray-300 textbtn" @click="notePlayer(props.player)">Note</button>
-                    <button type="button" class="bg-red-500 hover:bg-gray-300 textbtn" @click="showConfirmation = true">Delete</button>
-                </div>
-                <Box>
+                    <button type="button" class="bg-red-500 hover:bg-gray-300 textbtn" @click="showDeleteConfirmation = true">Delete</button>
+                </Box>
+                <Box class="m-1">
                     <div>
                         <h2 class="text-lg font-bold text">przeniesienie do innej gildii</h2>
                         <select v-model="selectedGuild">
@@ -16,12 +16,40 @@
                                 {{ guild.name }}
                             </option>
                         </select>
-                        <button class="bg-green-500 hover:bg-gray-300 textbtn" @click="transferPlayer(props.player)">Transfer</button>
+                        <button class="bg-green-500 hover:bg-gray-300 textbtn" @click="transferPlayer(props.player)" :disabled="!selectedGuild">Transfer</button>
                     </div>
+                </Box>
+                <Box class="m-1">
+                    <h2 class="text">Admin Tool</h2>
+                    <button type="button" class="bg-red-500 hover:bg-gray-300 textbtn" @click="showKickConfirmation = true">Kick</button>
+                    <button type="button" class="bg-red-500 hover:bg-gray-300 textbtn" @click="showBanConfirmation = true">Ban</button>
                 </Box>
                 <button type="button" class="bg-gray-500 hover:bg-gray-300 textbtn" @click="closeModal">Close</button>
             </div>
-            <div v-else>
+            <div v-else-if="showKickConfirmation">
+                <transition>
+                    <div>
+                        <h2 class="text-lg font-bold mb-2 dark:text-white">Are you sure?</h2>
+                        <button class="bg-red-500 hover:bg-gray-300 textbtn" @click="kickPlayer(props.player)">Yes</button>
+                        <button class="bg-blue-500 hover:bg-gray-300 textbtn" @click="showKickConfirmation = false">No</button>
+                    </div>
+                </transition>
+            </div>
+            <div v-else-if="showBanConfirmation">
+                <transition>
+                    <div>
+                        <h2 class="text-lg font-bold mb-2 dark:text-white">Are you sure you want to ban this player?</h2>
+                        <div class="form-group">
+                            <label for="name" class="grid justify-center text-center">opcjonalnie</label>
+                            <input id="name" v-model="form.uuid" type="text" class="input" placeholder="(wprowadź UUID)"/>
+                            <div v-if="form.errors.email" class="input.error">{{ form.errors.uuid }}</div>
+                        </div>
+                        <button class="bg-red-500 hover:bg-gray-300 textbtn" @click="() => banPlayer(props.player)">Yes</button>
+                        <button class="bg-blue-500 hover:bg-gray-300 textbtn" @click="showBanConfirmation = false">No</button>
+                    </div>
+                </transition>
+            </div>
+            <div v-else-if="showDeleteConfirmation">
                 <transition>
                     <div>
                         <h2 class="text-lg font-bold mb-2 dark:text-white">Are you sure?</h2>
@@ -39,6 +67,7 @@ import { router } from "@inertiajs/vue3";
 import { route } from "ziggy-js";
 import { ref } from 'vue';
 import Box from "@/Pages/Components/UI/Box.vue";
+import {useForm} from "@inertiajs/inertia-vue3";
 
 const props = defineProps({
     player: Object,
@@ -47,10 +76,16 @@ const props = defineProps({
     columnIndex: Number
 });
 
+const form = useForm({
+    uuid: null
+})
+
 const emit = defineEmits(['close']);
 
-let showConfirmation = ref(false);
-let selectedGuild = null
+let showDeleteConfirmation = ref(false);
+let showKickConfirmation = ref(false);
+let showBanConfirmation = ref(false);
+let selectedGuild = ref(null)
 
 
 const editPlayer = (player) => {
@@ -61,6 +96,21 @@ const notePlayer = (player) => {
     router.get(route('note.show', { note: player.user.id }));
 }
 
+const kickPlayer = (player) => {
+    router.delete(route('admin.user.destroy', { user: player.user.id, action: 'kick' }));
+    emit('close');
+}
+
+const banPlayer = (player) => {
+    console.log(player.user.id)
+    router.delete(route('admin.user.destroy', { user: player.user.id, action: 'ban' }), {
+        data: {
+            uuid: form.uuid
+        }
+    });
+    emit('close');
+}
+
 const deletePlayer = (player) => {
     router.delete(route('player.destroy', { player: player.id }));
     emit('close');
@@ -68,7 +118,7 @@ const deletePlayer = (player) => {
 
 const transferPlayer = (player) => {
     router.patch(route('players.transfer', { player: player.id }), {
-        guild_id: selectedGuild.id,
+        guild_id: selectedGuild.value.id,
         onSuccess: emit('close'),
         onError: console.error,
     });
@@ -83,7 +133,6 @@ const handleBackgroundClick = (event) => {
         closeModal();
     }
 }
-
 
 </script>
 
