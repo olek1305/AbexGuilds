08e2792f78af3b79b0a308952c72b9a72991c581
olek1305 <olek1305@gmail.com>
2024-04-02 11:16:47 +0200

    refactor

--- resources/js/Pages/Player/Index.vue
+++ resources/js/Pages/Player/Index.vue
@@ -1,36 +1,32 @@
 <template>
     <AppLayout title="Dashboard">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div>
-                    <Box class="rounded-b-none">
-                        <div v-if="isAdminUser" class="flex justify-center m-1">
-                            <Link class="flex btn justify-center w-52" :href="route('player.create')">Utwórz gracza</Link>
-                        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+            <Box class="rounded-b-none">
+                <div v-if="isAdminUser" class="flex justify-center m-1">
+                    <Link class="flex btn justify-center w-52" :href="route('player.create')">Utwórz gracza</Link>
+                </div>
 
-                        <PlayerFilters :filters="filters" :guilds="guilds" :seasons="seasons"/>
-                    </Box>
-                    <div class="overflow-x-auto">
-                        <table class="table-auto text-2xl w-full text-left text-gray-500 dark:text-gray-400">
-                            <tr class="text-xl text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <th class="text-center" v-for="(column, index) in columns" :key="index">
-                                    {{ column }}
-                                </th>
-                            </tr>
-                                <Player
-                                    v-for="(player, index) in players.data"
-                                    :key="player.id"
-                                    :player="player"
-                                    :columnIndex="index"
-                                    :guilds="guilds"
-                                />
-                        </table>
-                    </div>
-                </div>
-                <div v-if="players.data.length" class="w-full flex justify-center mt-4 mb-4 dark:text-gray-400">
-                    <Pagination :links="players.links"/>
-                </div>
+                <PlayerFilters :filters="filters" :guilds="guilds" :seasons="seasons"/>
+            </Box>
+            <div class="overflow-x-auto">
+                <table class="table-auto text-2xl w-full text-left text-gray-500 dark:text-gray-400">
+                    <tr class="text-xl text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <th class="text-center" v-for="(column, index) in columns" :key="index">
+                            {{ column }}
+                        </th>
+                    </tr>
+                        <Player
+                            v-for="(player, index) in players.data"
+                            :key="player.id"
+                            :player="player"
+                            :columnIndex="index"
+                            :guilds="guilds"
+                        />
+                </table>
             </div>
+        </div>
+        <div v-if="players.data.length" class="w-full flex justify-center mt-4 mb-4 dark:text-gray-400">
+            <Pagination :links="players.links"/>
         </div>
     </AppLayout>
 </template>
