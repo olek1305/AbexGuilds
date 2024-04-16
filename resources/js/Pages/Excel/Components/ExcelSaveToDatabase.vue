87765a1f030ab2875678e6446c69bcdc64c19c48
olek1305 <olek1305@gmail.com>
2024-04-16 20:51:21 +0200

    remove id

--- resources/js/Pages/Excel/Components/ExcelSaveToDatabase.vue
+++ resources/js/Pages/Excel/Components/ExcelSaveToDatabase.vue
@@ -3,7 +3,7 @@
         <p class="title pb-2">Kopiowanie dane z Excela w celu zapisania w bazie danych (Player i inne dane)</p>
         <p class="text-rose-300">UWAGA: MUSI BYC DOKLADNIE NAZW KOLUMNY W EXCELU TAKI JAK PONIZEJ:</p>
         <p class="text-rose-300">WARNING: MUST BE EXACTLY THE COLUMN NAME IN EXCEL LIKE AS BELOW:</p>
-        <p class="text">| id | user_name | damage | guild | is_observer | is_star | season |</p>
+        <p class="text">| user_name | damage | guild | is_observer | is_star | season |</p>
         <form @submit.prevent="submit">
             <input v-model="form.sheetName" type="text" placeholder="Nazwa sheet">
             <button type="submit" class="text p-2 btn">Wyślij</button>
